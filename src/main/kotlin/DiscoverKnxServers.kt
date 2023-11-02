a257bd2429deb350e5e08b04e97c961690afa689
bmalinowsky <bmalinowsky@github.com>
2023-11-02 18:08:16 +0100

    Replace deprecated method

--- src/main/kotlin/DiscoverKnxServers.kt
+++ src/main/kotlin/DiscoverKnxServers.kt
@@ -15,8 +15,8 @@ fun main() {
         val useNAT = false
         Discoverer.udp(useNAT).timeout(Duration.ofSeconds(3)).search().get()
             .forEach {
-                val res = it.response.toString().replace(", ", "\n\t")
-                println("${it.networkInterface.name} ${it.localEndpoint()} <=> $res")
+                val res = it.response().toString().replace(", ", "\n\t")
+                println("${it.networkInterface().name} ${it.localEndpoint()} <=> $res")
             }
     } catch (e: InterruptedException) {
         System.err.println("Error during KNXnet/IP discovery: $e")
