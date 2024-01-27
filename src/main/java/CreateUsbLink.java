bc974aa21c796606c93a750301b782aaa8c7ab95
bmalinowsky <bmalinowsky@github.com>
2024-01-27 17:57:34 +0100

    Add listener for knx connection status

--- src/main/java/CreateUsbLink.java
+++ src/main/java/CreateUsbLink.java
@@ -1,6 +1,6 @@
 /*
     Calimero 2 - A library for KNX network access
-    Copyright (c) 2016, 2023 B. Malinowsky
+    Copyright (c) 2016, 2024 B. Malinowsky
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -20,14 +20,17 @@
 import io.calimero.KNXException;
 import io.calimero.link.KNXNetworkLink;
 import io.calimero.link.KNXNetworkLinkUsb;
+import io.calimero.link.LinkEvent;
+import io.calimero.link.NetworkLinkListener;
 import io.calimero.link.medium.TPSettings;
+import io.calimero.serial.ConnectionStatus;
 
 /**
  * This example shows how to establish a client network link ({@link KNXNetworkLink}) to a KNX TP1 network using a KNX
  * USB device. Minimum requirements are Calimero version 3.0-SNAPSHOT and Java SE 17 (java.base).
  * <p>
- * You can safely run this example; the (established) connection is closed directly afterwards. No KNX messages are sent
- * to the KNX network.
+ * You can safely run this example; the established connection is closed 10 seconds after creation.
+ * No KNX messages are sent to the KNX network.
  *
  * @author B. Malinowsky
  */
@@ -46,7 +49,15 @@ public class CreateUsbLink
 		// indicates that the KNX installation uses twisted-pair (TP) medium, with TP1 being most common.
 		try (KNXNetworkLink knxLink = new KNXNetworkLinkUsb(device, new TPSettings())) {
 			System.out.println("Connection established using KNX USB device " + knxLink.getName());
-			System.out.println("Link status: connected=" + knxLink.isOpen());
+
+			// Add a listener with a link event which notifies us in case the USB interface to KNX connection got disrupted.
+			// (Note, this is not the connection-state of the USB network link attached to this host itself.)
+			knxLink.addLinkListener(new NetworkLinkListener() {
+				@LinkEvent
+				void status(ConnectionStatus status) { System.out.println("KNX connection status: " + status); }
+			});
+
+			Thread.sleep(10_000);
 		}
 		catch (KNXException | InterruptedException e) {
 			// KNXException: all Calimero-specific checked exceptions are subtypes of KNXException
