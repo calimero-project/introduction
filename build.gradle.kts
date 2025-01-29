7222ce78ab417e52481fa9a268b1419fe586665d
bmalinowsky <bmalinowsky@github.com>
2025-01-29 14:21:16 +0100

    Upgrade to versions 0.52.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
 	kotlin("jvm") version "2.0.20"
 	application
-	id("com.github.ben-manes.versions") version "0.51.0"
+	id("com.github.ben-manes.versions") version "0.52.0"
 	`maven-publish`
 	signing
 }
