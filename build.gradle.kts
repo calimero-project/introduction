f5f45ecec11b4232c04dfafcf98c1e0a12b4f3be
bmalinowsky <bmalinowsky@github.com>
2025-06-23 16:32:21 +0200

    Upgrade to kotlin jvm 2.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
-	kotlin("jvm") version "2.1.20"
+	kotlin("jvm") version "2.2.0"
 	application
 	id("com.github.ben-manes.versions") version "0.52.0"
 	`maven-publish`
